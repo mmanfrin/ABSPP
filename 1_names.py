ae7ed70d99b62837bd6cd3d74e25f9c0bd9beba2
MArcos Manfrin <mmanfrin2@gmail.com>
2019-11-16 18:26:45 -0300

    16-11-1

---
+++ 1_names.py
@@ -0,0 +1,7 @@
+# This programm says hello and asks for my name
+
+myName = input('Hello world!\nWhat is your name?')
+print(f"It's good to meet you, {myName}\nThe lenght of your name is: {len(myName)}")
+
+myAge = int(input('What is your age?'))
+print(f'You will be {str(int(myAge)+1)} in a year!')
